ce3da92930a99d21652cd76f884a1690dc2e5aa4
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2021-11-18 10:13:24 -0700

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,13 +1,29 @@
+// Sort Analyzer Start Code
+
 using System;
 using System.IO;
+using System.Diagnostics;
 
 class MainClass {
     public static void Main (string[] args) {
-        // Load Data Files into Arrays
+        // LOAD DATA FILES INTO ARRAYS
         int[] randomData = loadDataArray("data-files/random-values.txt");
         int[] reversedData = loadDataArray("data-files/reversed-values.txt");
         int[] nearlySortedData = loadDataArray("data-files/nearly-sorted-values.txt");
         int[] fewUniqueData = loadDataArray("data-files/few-unique-values.txt");
+        
+        // VERIFY LOADED DATA BY PRINTING 50 ELEMENTS
+        printIntArray(randomData, 0, 50);
+        printIntArray(reversedData, 0, 50);
+        printIntArray(nearlySortedData, 0, 50);
+        printIntArray(fewUniqueData, 0, 50);
+
+        // EXAMPLE OF TIMING DURATION OF A SORT ALGORITHM
+        // var timer = new Stopwatch();
+        // timer.Start();
+        // bubbleSort(randomData);
+        // timer.Stop();
+        // Console.WriteLine($"Bubble Sort Random Data: {timer.Elapsed}");
     }
 
     // Function to create an array of integers from provided data file
@@ -23,5 +39,12 @@ class MainClass {
 
         // Return Array of Integers
         return tempData;
+    }
+    
+    public static void printIntArray(int[] array, int start, int stop) {
+        // Print out array elements at index values from start to stop 
+        for (int i = start; i < stop; i++) {
+            Console.WriteLine(array[i]);
+        }
     }
 }
